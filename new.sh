585e17fa20a6a67be70d6f4d5dd894074636191f
JNDTEAM <62438778+JNDOFFICIAL@users.noreply.github.com>
2021-02-12 17:06:08 +0700

    Add files via upload

---
+++ new.sh
@@ -0,0 +1,17 @@
+
+pkg install update
+pkg install upgrade
+pkg install nano
+pkg install php git
+pkg install termux-api imagemagick
+pkg install ncurses-utils
+pkg install python python2
+pkg install pip
+pip install --upgrade pip
+pip install rsa bs4 requests colorama async_generator
+pkg install ruby cowsay toilet figlet
+ pip install cbpro
+ pip install requests
+ pip install telethon
+ pip install pyaes
+ termux-setup-storage
